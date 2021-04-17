32cb67707d45336c05da6b061fe081e0ab36b274
OlegDemkiv <olegdemkiv111@gmail.com>
2021-04-17 15:17:00 +0300

    Aded coments to code

--- linked_list.c
+++ linked_list.c
@@ -30,24 +30,24 @@ void push_front(t_list **list, int set_id, char *set_name)			// Додаванн
 {
 	t_list *new_element = create_node(set_id, set_name);			// Створення нового елемента
 
-	new_element -> next = *list;									// перевизначення змінної next на адрес
-	*list = new_element;			// ???
+	new_element -> next = *list;									// перевизначення поля next на попередній адрес
+	*list = new_element;			// присвоїти адрес даного елемента списку змінній list
 }
 // -------------------------------------------------------------------------
 void linked_list(void)
 {
-	t_list *list = create_node(0, "Name_1");						// Створити один связний список
+	t_list *list = create_node(0, "Name_1");		// Створити один связний список
 
 	// Додавання наступного елементу списку на початок.
-	push_front(&list, 1, "Name_2");			// передається адрес попереднього списку.
+	push_front(&list, 1, "Name_2");					// передається адрес попереднього списку.
 	push_front(&list, 2, "Name_3");
 	push_front(&list, 3, "Name_4");
 	push_front(&list, 4, "Name_5");
 
-	while(list != NULL)
+	while(list != NULL)								// Друкувати елементи допоки вони не ссилаються на NULL
 	{
 		printf("id = %d, name = %s\n", list -> id, list -> name);
-		list = list -> next;
+		list = list -> next;						// Запис в list показник на наступний елемент списку
 	}
 }
 // -------------------------------------------------------------------------
