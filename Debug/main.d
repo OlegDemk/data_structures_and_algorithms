e48816f28b928a354fced92075479f9c5e6fce07
OlegDemkiv <olegdemkiv111@gmail.com>
2021-04-17 14:44:57 +0300

    Updated files

--- Debug/main.d
+++ Debug/main.d
@@ -1,5 +1,7 @@
-main.o: ../main.c ../queues.h ../example_of_queues.h
+main.o: ../main.c ../queues.h ../example_of_queues.h ../linked_list.h
 
 ../queues.h:
 
 ../example_of_queues.h:
+
+../linked_list.h:
--- linked_list.h
+++ linked_list.h
@@ -8,6 +8,6 @@
 #ifndef LINKED_LIST_H_
 #define LINKED_LIST_H_
 
-
+void linked_list(void);
 
 #endif /* LINKED_LIST_H_ */
